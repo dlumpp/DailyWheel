1a75ed9e96254e7823b897246eee74d249965a5f
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2021-04-02 10:20:37 -0400

    fix warnings in ErrorModel

--- DailyWheel/Pages/Error.cshtml.cs
+++ DailyWheel/Pages/Error.cshtml.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DailyWheel.Pages
 {
@@ -13,15 +8,12 @@ namespace DailyWheel.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
-
-        public ErrorModel(ILogger<ErrorModel> logger)
+        public ErrorModel()
         {
-            _logger = logger;
         }
 
         public void OnGet()
