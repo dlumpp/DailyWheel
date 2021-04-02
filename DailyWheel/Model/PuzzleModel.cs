8e6120da2d5f029175003a7af344a1161c9eaf50
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2021-04-02 10:16:55 -0400

    don't put prerender view in edit mode

--- DailyWheel/Model/PuzzleModel.cs
+++ DailyWheel/Model/PuzzleModel.cs
@@ -6,10 +6,15 @@ namespace DailyWheel.Model
     {
         public Guid? Id { get; set; }
         public string? Day { get; set; }
-        public DateTime? Date => DateTime.TryParse(Day, out var result) ? result : null;
         public string? Category { get; set; }
         public char?[,] Board { get; set; } = new char?[4, 14];
         public string? UsedLetters { get; set; }
+                
+        public DateTime? ParseDay() => DateTime.TryParse(Day, out var result) ? result : null;
+
+        public bool IsUnsaved() => !Id.HasValue;
+
+        public bool IsPrerenderInstance() => this == PrerenderPlaceholder;
 
         public static readonly PuzzleModel PrerenderPlaceholder = new();
     }
