ff69cc887190eabcce199fc2441cb55e99a3cc63
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2021-04-02 10:47:30 -0400

    show prerender model w loading animation while waiting for puzzle fetch

--- DailyWheel/Model/PuzzleModel.cs
+++ DailyWheel/Model/PuzzleModel.cs
@@ -9,13 +9,18 @@ namespace DailyWheel.Model
         public string? Category { get; set; }
         public char?[,] Board { get; set; } = new char?[4, 14];
         public string? UsedLetters { get; set; }
-                
+
         public DateTime? ParseDay() => DateTime.TryParse(Day, out var result) ? result : null;
 
         public bool IsUnsaved() => !Id.HasValue;
 
         public bool IsPrerenderInstance() => this == PrerenderPlaceholder;
 
-        public static readonly PuzzleModel PrerenderPlaceholder = new();
+        public static readonly PuzzleModel PrerenderPlaceholder = new()
+        {
+            Day = DateTime.Now.ToString("yyyy-MM-dd"),
+            Category = "What are you doing?",
+            UsedLetters = "RSTLNE ABCD"
+        };
     }
 }
