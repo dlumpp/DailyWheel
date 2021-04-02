476d769abbf9e6f26de1ac0347845b6a44099071
Dan Lumpp <20948500+dlumpp@users.noreply.github.com>
2021-04-02 09:46:35 -0400

    working MVP

---
+++ DailyWheel/Data/PuzzleRepository.cs
@@ -0,0 +1,47 @@
+﻿using DailyWheel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace DailyWheel.Data
+{
+    interface IPuzzleRepository
+    {
+        Task<PuzzleModel?> GetAsync(string day);
+        Task SaveAsync(PuzzleModel puzzleModel);
+    }
+
+    public class PuzzleRepository : IPuzzleRepository
+    {
+        private readonly Container container;
+
+        public PuzzleRepository(CosmosClient cosmosClient)
+        {
+            container = cosmosClient.GetContainer("DailyWheel", "Puzzles");
+        }
+
+        public async Task<PuzzleModel?> GetAsync(string day)
+        {
+            using var queryIterator = container.GetItemLinqQueryable<PuzzleModel?>()
+                .Where(p => p != null && p.Day == day)
+                .ToFeedIterator();
+
+            var results = new List<PuzzleModel?>();
+            while (queryIterator.HasMoreResults)
+                foreach (var i in await queryIterator.ReadNextAsync())
+                    results.Add(i);
+
+            return results.SingleOrDefault();
+
+        }
+
+        public async Task SaveAsync(PuzzleModel puzzleModel)
+        {
+            await container.CreateItemAsync(puzzleModel, new PartitionKey(puzzleModel.Day));
+        }
+    }
+}
---
+++ DailyWheel/Model/PuzzleModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DailyWheel.Model
+{
+    public class PuzzleModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Day { get; set; }
+        public DateTime? Date => DateTime.TryParse(Day, out var result) ? result : null;
+        public string Category { get; set; }
+        public char?[,] Board { get; set; } = new char?[4, 14];
+        public string UsedLetters { get; set; }
+    }
+}
